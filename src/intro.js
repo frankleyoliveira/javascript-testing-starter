e72d16d73b5bce0b7eefa901f64e11da27674278
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-05 13:25:12 -0300

    exercise write factorial function using TDD

--- src/intro.js
+++ src/intro.js
@@ -18,3 +18,10 @@ export function calculateAverage(numbers) {
 
   return sum / numbers.length
 }
+
+export function factorial(number) {
+  if (number < 0) return undefined
+  if (number === 0 || number === 1) return 1
+
+  return number * factorial(number - 1)
+}
